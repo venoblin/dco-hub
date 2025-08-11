d8e2842df8666bc256386d7acbb452cd561e8fc1
Jonathan <villagomezhjonathan@gmail.com>
2025-08-10 23:57:28 -0400

    first commit of files

---
+++ src/main/java/com/venoblin/dcohub/DcohubApplication.java
@@ -0,0 +1,13 @@
+package com.venoblin.dcohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcohubApplication.class, args);
+	}
+
+}
