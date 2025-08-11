103d16b316414e6caffb5be87e5f4a7239a0559f
Jonathan <villagomezhjonathan@gmail.com>
2025-08-11 13:12:24 -0400

    fixed white space

--- src/main/java/com/venoblin/dcohub/DcohubApplication.java
+++ src/main/java/com/venoblin/dcohub/DcohubApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class DcohubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DcohubApplication.class, args);
 	}
-
 }
--- src/main/java/com/venoblin/dcohub/controller/api/GuideController.java
+++ src/main/java/com/venoblin/dcohub/controller/api/GuideController.java
@@ -1,16 +1,13 @@
 package com.venoblin.dcohub.controller.api;
 
-import java.util.List;
-
+import com.venoblin.dcohub.model.Guide;
+import com.venoblin.dcohub.service.GuideService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.venoblin.dcohub.model.Guide;
-import com.venoblin.dcohub.service.GuideService;
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/guides")
