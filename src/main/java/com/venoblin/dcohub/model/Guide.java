600fc84eca4222f0c6acd4a57d032fe21d32b14f
Jonathan <villagomezhjonathan@gmail.com>
2025-08-11 03:14:45 -0400

    finished guide class

--- src/main/java/com/venoblin/dcohub/model/Guide.java
+++ src/main/java/com/venoblin/dcohub/model/Guide.java
@@ -3,7 +3,6 @@ package com.venoblin.dcohub.model;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
