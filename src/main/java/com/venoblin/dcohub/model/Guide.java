597c3b88a410c8a4a2eaa8cea55209c84db5382d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-08-17 21:59:12 -0400

    added setter and getter methods

--- src/main/java/com/venoblin/dcohub/model/Guide.java
+++ src/main/java/com/venoblin/dcohub/model/Guide.java
@@ -15,23 +15,38 @@ public class Guide {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-
   @Column(nullable = false)
   private String author;
-
   @Column(nullable = false)
   private String title;
-  
   @Column(columnDefinition = "TEXT", nullable = false)
   private String content;
-
   @ElementCollection
   private List<String> imagePaths = new ArrayList<>();
-
   @CreatedDate
   @Column(updatable = false)
   private LocalDateTime createdAt;
-
   @LastModifiedDate
   private LocalDateTime updatedAt;
+
+  public Guide() {}
+
+  public String getAuthor() {
+    return author;
+  }
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
 }
