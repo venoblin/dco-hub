6d30d2712317724f4b3c9bc13bfc2d5ecb586e02
Jonathan <villagomezhjonathan@gmail.com>
2025-08-19 20:40:37 -0400

    worked on new guide script

--- src/main/resources/static/scripts/newGuide.js
+++ src/main/resources/static/scripts/newGuide.js
@@ -1,1 +1,11 @@
-console.log('new guide')
+const guideForm = document.getElementById('new-guide-form')
+
+guideForm.addEventListener('submit', async (event) => {
+  event.preventDefault()
+
+  const guideAuthor = document.getElementById('author').value
+  const guideTitle = document.getElementById('title').value
+  const guideContent = document.getElementById('content').value
+
+  console.log(guideAuthor, guideTitle, guideContent)
+})
