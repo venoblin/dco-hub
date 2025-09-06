8a0ef8bbe228f8494e87c58821dd656dd4e01a14
Jonathan <villagomezhjonathan@gmail.com>
2025-09-06 13:03:10 -0400

    adde class methods

--- src/main/resources/static/scripts/repository/client.js
+++ src/main/resources/static/scripts/repository/client.js
@@ -4,4 +4,21 @@ class Client {
   constructor(baseUrl) {
     this.#baseUrl = baseUrl
   }
+
+  #constructUrl = (endpoint) => {
+    let cleanedEndpoint = endpoint
+
+    if (cleanedEndpoint[0] === '/') {
+      cleanedEndpoint.splice(0, 1)
+    }
+    console.log(cleanedEndpoint)
+
+    return `${this.#baseUrl}/${cleanedEndpoint}`
+  }
+
+  async get(endpoint) {
+    try {
+      const res = await fetch()
+    } catch (error) {}
+  }
 }
