0978fc05763714c5bbc19dd7f74043f9e5efba11
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-08 19:38:26 -0400

    worked on single guide

--- src/main/java/com/venoblin/dcohub/controller/web/RoutesController.java
+++ src/main/java/com/venoblin/dcohub/controller/web/RoutesController.java
@@ -3,6 +3,7 @@ package com.venoblin.dcohub.controller.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RoutesController {
@@ -23,7 +24,9 @@ public class RoutesController {
   }
 
   @GetMapping("/guides/{id}")
-  public String singleGuide(Model model) {
+  public String singleGuide(@PathVariable("id") Long id, Model model) {
+    model.addAttribute("guideId", id);
+    
     return "singleGuide";
   }
 }
