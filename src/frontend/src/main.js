1a3413aa8b59bd54da7b68ab4c6ac79856a8d0ec
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-10 18:39:38 -0400

    implemented quil

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,11 +1,12 @@
 import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
 
 const toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'], // toggled buttons
   ['blockquote', 'code-block'],
   ['link', 'image', 'video', 'formula'],
 
-  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ header: 1 }, { header: 2 }, { header: 3 }], // custom button values
   [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
   [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
   [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
@@ -15,7 +16,7 @@ const toolbarOptions = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
   [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-  [{ font: [] }],
+  // [{ font: [] }],
   [{ align: [] }],
 
   ['clean'] // remove formatting button
