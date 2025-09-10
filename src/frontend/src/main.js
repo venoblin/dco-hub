6d5e03e97430b3eb11d82e352486f00da22d33a6
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-10 16:49:04 -0400

    worked on editor

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,9 +1,30 @@
-import EditorJS from '@editorjs/editorjs'
-import Header from '@editorjs/header'
+import Quill from 'quill'
 
-export const editor = new EditorJS({
-  holder: 'editor-container',
-  tools: {
-    header: Header
-  }
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+  ['blockquote', 'code-block'],
+  ['link', 'image', 'video', 'formula'],
+
+  [{ header: 1 }, { header: 2 }], // custom button values
+  [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+  [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+  [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+  [{ direction: 'rtl' }], // text direction
+
+  [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ['clean'] // remove formatting button
+]
+
+export const quill = new Quill('#editor-container', {
+  modules: {
+    toolbar: toolbarOptions
+  },
+  theme: 'snow',
+  placeholder: 'Content'
 })
