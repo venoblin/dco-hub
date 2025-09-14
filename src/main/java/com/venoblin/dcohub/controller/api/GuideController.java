811db66f3c26b6e86f36647dc561f925b352a485
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-14 13:04:07 -0400

    commit

--- src/main/java/com/venoblin/dcohub/controller/api/GuideController.java
+++ src/main/java/com/venoblin/dcohub/controller/api/GuideController.java
@@ -5,6 +5,7 @@ import com.venoblin.dcohub.service.GuideService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
