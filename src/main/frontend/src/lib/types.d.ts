3a120a9af1ddd1015f7c8b6a25479584505f5d8c
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-19 16:38:26 -0400

    readded svelte

---
+++ src/main/frontend/src/lib/types.d.ts
@@ -0,0 +1,12 @@
+interface GuidePayload {
+  author: string
+  title: string
+  content: string
+  shortDescription: string
+}
+
+interface Guide extends GuidePayload {
+  id: number
+  createdAt: string
+  updatedAt: string
+}
